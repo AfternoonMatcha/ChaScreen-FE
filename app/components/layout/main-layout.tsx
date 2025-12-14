ee149c21983f081f265fcfee4d14fb9b3bdfb83c
午后抹茶酱 Matce.CN <Matce@qq.com>
2025-12-14 20:10:48 +0800

    feat(layout): 修改主布局结构、更新首页内容
    - 调整 MainLayout 组件，使用 flex 布局、移除多余 main 标签
    - 更新 Home 页面，替换原有计数器、添加影厅选择界面
    - 移除未使用的 Button 组件、useState hook

--- app/components/layout/main-layout.tsx
+++ app/components/layout/main-layout.tsx
@@ -4,15 +4,13 @@ import Logo from '~/assets/images/LOGO.svg';
 
 export function MainLayout() {
   return (
-    <div className="min-h-screen bg-background">
+    <div className="min-h-screen bg-background flex flex-col">
 
       <header className="sticky top-0 z-50 w-full h-[70px] bg-card">
         <img src={Logo} alt="Logo" />
       </header>
 
-      <main className="h-full">
-        <Outlet />
-      </main>
+      <Outlet />
 
     </div>
   );
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,3 @@
-import { Button } from "~/components/ui/button"
-import { useState } from "react"
-
 import type { Route } from "./+types/home"
 
 // 定义页面的元数据函数
@@ -11,14 +8,15 @@ export function meta({ }: Route.MetaArgs) {
   ]
 }
 
-// 默认导出主页组件
 export default function Home() {
-  const [count, setCount] = useState(0)
-
-  return ( // 返回 JSX 元素
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <div className="text-4xl font-bold mb-4">{count}</div>
-      <Button className="cursor-pointer" variant="outline" onClick={() => setCount(count + 1)}>点击增加计数</Button>
-    </div>
+  return (
+    <main className="flex-1 p-[10px] flex gap-[10px] flex-col md:flex-row">
+      <article className="min-w-[300px] border rounded-[5px] p-[20px] pt-[30px]">
+        <div className="text-[23px]">选择影厅</div>
+      </article>
+      <article className="flex-1 border rounded-[5px] p-[20px] pt-[30px]">
+        <div className="text-[23px]">CINITY LED 杜比全景声厅 沙发软座</div>
+      </article>
+    </main>
   )
 }
