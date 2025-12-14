00585fd20a333458c8d4a80cb2da9aa9b7d237e2
午后抹茶酱 Matce.CN <Matce@qq.com>
2025-12-15 01:11:22 +0800

    fix: 移除了 Card 组件中冗余的 ref 和 props 处理逻辑

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -1,21 +1,17 @@
 import React from "react";
 import { cn } from "@/lib/utils";
 
-// 在 React 19 中，ref 变成一个普通的 prop
 interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
   title: string;
-  ref?: React.Ref<HTMLDivElement>; // 手动添加 ref prop
 }
 
-export default function Card({ title, children, className, ref, ...props }: CardProps) {
+export default function Card({ title, children, className }: CardProps) {
   return (
     <div
-      ref={ref} // 直接使用解构出的 ref prop
       className={cn(
         "rounded-[5px] border",
         className
       )}
-      {...props}
     >
       <div className="flex flex-col p-[20px] pt-[30px]">
         <div className="text-[23px]">
